38268c8ed29d2574d48970188a027a188c9e80d4
slowrunner <slowrunner@users.noreply.gethub.com>
2022-06-16 12:34:51 -0400

    doc parms

--- picam2/capture_headless.py
+++ picam2/capture_headless.py
@@ -4,7 +4,29 @@ from picamera2 import Picamera2
 import libcamera
 
 picam2 = Picamera2()
-config = picam2.still_configuration(transform=libcamera.Transform(hflip=1, vflip=1))
+
+# configure only one parameter when creating config
+# config = picam2.still_configuration(transform=libcamera.Transform(hflip=1, vflip=1))
+# config = picam2.still_configuration(main={"size": (320, 240)})
+
+# configure multiple parameters when creating config
+config = picam2.still_configuration( \
+                    main={"size": (320, 240)}, \
+                    transform=libcamera.Transform(hflip=1, vflip=1) \
+                    )
+
+# configure parameters after creating config
+# config = picam2.still_configuration()
+# config["transform"] = libcamera.Transform(hflip=1, vflip=1)
+# config["main"]["size"] = (320, 240)
+
+# print them to be sure method result is same
+print("config:",config)
+
+# transform is a libcamera class object, not a json string
+print('config["transform"].hflip', config["transform"].hflip)
+print('config["transform"].vflip', config["transform"].vflip)
+
 picam2.configure(config)
 
 picam2.start()
